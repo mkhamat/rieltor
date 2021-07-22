a1565370b56e3682cc2ab61f009167f74b377bd1
mkhamat <myxa@xakep.org>
2021-07-22 19:39:57 +0300

    fix

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,12 @@ import mongoose from "mongoose"
 import appartmentRoutes from "./routes/appartmentRoutes.js"
 import uploadRoutes from "./routes/imageRoutes.js"
 import userRoutes from "./routes/userRoutes.js"
+import path from "path"
+import { fileURLToPath } from "url"
 import dotenv from "dotenv"
 dotenv.config()
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 const db = mongoose.connection
 db.once("open", () => {
@@ -20,14 +24,13 @@ mongoose.connect(process.env.MONGO_URI, {
 })
 
 app.use(express.json())
-app.use("/static", express.static("images"))
-app.use(express.static("../client/build"))
+app.use("/static", express.static(path.join(__dirname, "./images")))
+app.use(express.static(path.join(__dirname, "../client/build")))
 
 app.use("/appartments", appartmentRoutes)
 app.use("/users", userRoutes)
 app.use("/images", uploadRoutes)
-
 app.get("*", (req, res) => {
-  res.sendFile("../client/build/index.html")
+  res.sendFile(path.join(__dirname, "../client/build/index.html"))
 })
-app.listen(3001)
+app.listen(process.env.PORT)
