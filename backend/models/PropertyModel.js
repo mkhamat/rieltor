ae9799a49f580b9391ace74d902a37dd6f9ad1aa
mkhamat <myxa@xakep.org>
2021-08-10 11:39:47 +0300

    add new property to schema

--- backend/models/PropertyModel.js
+++ backend/models/PropertyModel.js
@@ -1,38 +1,39 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
 const propertySchema = new mongoose.Schema({
-    address: {
-        type: String,
-        required: true,
-    },
-    price_type: {
-        type: String,
-    },
-    price: {
-        type: Number,
-    },
-    property_type: 'flat' | 'house' | 'land',
-    rooms: {
-        type: Number,
-    },
-    floor: {
-        type: Number,
-    },
-    floors: {
-        type: Number,
-    },
-    m2: {
-        type: Number,
-    },
-    description: {
-        type: String,
-    },
-    phone: {
-        type: Number,
-    },
-    pictures: [],
-})
+  address: {
+    type: String,
+    required: true,
+  },
+  price_type: {
+    type: String,
+  },
+  price: {
+    type: Number,
+  },
+  property_type: String,
+  property_object: String,
+  rooms: {
+    type: Number,
+  },
+  floor: {
+    type: Number,
+  },
+  floors: {
+    type: Number,
+  },
+  m2: {
+    type: Number,
+  },
+  description: {
+    type: String,
+  },
+  phone: {
+    type: Number,
+  },
+  pictures: [],
+});
 
-propertySchema.index({address: "text", description: "text"})
-const Property = mongoose.model("Appartment", propertySchema)
-export {Property}
+propertySchema.index({ address: "text", description: "text" });
+const Property = mongoose.model("Appartment", propertySchema);
+export { Property };
