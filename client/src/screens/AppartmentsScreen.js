48dec1532598858c473887e951c85a63b9ca85a4
mkhamat <myxa@xakep.org>
2021-08-16 23:28:54 +0300

    Show more on screen and hide filters and search

--- client/src/screens/AppartmentsScreen.js
+++ client/src/screens/AppartmentsScreen.js
@@ -1,6 +1,7 @@
 import {
   BackTop,
   Col,
+  Collapse,
   Input,
   InputNumber,
   Pagination,
@@ -29,7 +30,7 @@ export default function AppartmentScreen({ user }) {
   const [pageState, setPageState] = useState({
     current: 1,
     count: 0,
-    limit: 6,
+    limit: 12,
   })
 
   const handleChange = (p) => {
@@ -77,105 +78,113 @@ export default function AppartmentScreen({ user }) {
         current={pageState.current}
         pageSize={pageState.limit}
         total={pageState.count}
+        responsive={true}
       />
-      <Input
-        style={{ margin: "1em 0", width: 150 }}
-        placeholder="Общий поиск"
-        onChange={(e) => {
-          setFilter({ ...filter, search: e.target.value })
-        }}
-      />
-      <Radio.Group
-        onChange={(e) => {
-          setFilter({ ...filter, price_type: e.target.value })
-        }}
-      >
-        <Radio.Button value="rent">Аренда</Radio.Button>
-        <Radio.Button value="sell">Продажа</Radio.Button>
-      </Radio.Group>
-      <Input.Group>
-        <Select
-          options={selectOptions}
-          style={{ width: "100%" }}
-          placeholder="Типы объекта"
-          onChange={(value) =>
-            setFilter({
-              ...filter,
-              property_object: value.length ? value : null,
-            })
-          }
-          mode={"multiple"}
-          allowClear
-        />
-      </Input.Group>
-      <Input.Group>
-        <InputNumber
-          placeholder="Комнаты"
-          style={{ margin: "1em 0", width: 100 }}
-          // allowClear
-          onChange={(value) => {
-            setFilter({ ...filter, rooms: value })
-          }}
-        />
-        <InputNumber
-          min={1}
-          type="number"
-          maxLength={3}
-          style={{ margin: "1em 0", width: 100 }}
-          placeholder="Этаж"
-          onChange={(value) => {
-            setFilter({ ...filter, floor: value })
-          }}
-        />
-      </Input.Group>
-      <Input.Group>
-        <InputNumber
-          min={1}
-          type="number"
-          maxLength={8}
-          style={{ margin: "1em 0", width: 100 }}
-          placeholder="Площадь >"
-          onChange={(value) => {
-            setFilter({ ...filter, m2: value })
-          }}
-        />
-        <InputNumber
-          min={1}
-          type="number"
-          maxLength={8}
-          style={{ margin: "1em 0", width: 100 }}
-          placeholder="< Площадь"
-          onChange={(value) => {
-            setFilter({ ...filter, m22: value })
-          }}
-        />
-      </Input.Group>
-      <Input.Group>
-        <InputNumber
-          min={1}
-          type="number"
-          style={{ margin: "1em 0", width: 100 }}
-          placeholder="Цена >"
-          onChange={(value) => {
-            setFilter({ ...filter, priceFrom: value })
-          }}
-        />
-        <InputNumber
-          min={1}
-          type="number"
-          style={{ margin: "1em 0", width: 100 }}
-          placeholder="< Цена"
-          onChange={(value) => {
-            setFilter({ ...filter, priceTo: value })
-          }}
-        />
-      </Input.Group>
+      <Collapse ghost>
+        <Collapse.Panel header="Поиск и фильтрация">
+          <Input
+            style={{ margin: "1em 0", width: 150 }}
+            placeholder="Общий поиск"
+            onChange={(e) => {
+              setFilter({ ...filter, search: e.target.value })
+            }}
+          />
+          <Radio.Group
+            style={{ marginBottom: "1rem" }}
+            onChange={(e) => {
+              setFilter({ ...filter, price_type: e.target.value })
+            }}
+          >
+            <Radio.Button value="rent">Аренда</Radio.Button>
+            <Radio.Button value="sell">Продажа</Radio.Button>
+          </Radio.Group>
+          <Input.Group>
+            <Select
+              options={selectOptions}
+              style={{ width: "100%" }}
+              placeholder="Типы объекта"
+              onChange={(value) =>
+                setFilter({
+                  ...filter,
+                  property_object: value.length ? value : null,
+                })
+              }
+              mode={"multiple"}
+              allowClear
+            />
+          </Input.Group>
+          <Input.Group>
+            <InputNumber
+              placeholder="Комнаты"
+              style={{ margin: "1em 0", width: 100 }}
+              // allowClear
+              onChange={(value) => {
+                setFilter({ ...filter, rooms: value })
+              }}
+            />
+            <InputNumber
+              min={1}
+              type="number"
+              maxLength={3}
+              style={{ margin: "1em 0", width: 100 }}
+              placeholder="Этаж"
+              onChange={(value) => {
+                setFilter({ ...filter, floor: value })
+              }}
+            />
+          </Input.Group>
+          <Input.Group>
+            <InputNumber
+              min={1}
+              type="number"
+              maxLength={8}
+              style={{ margin: "1em 0", width: 100 }}
+              placeholder="Площадь >"
+              onChange={(value) => {
+                setFilter({ ...filter, m2: value })
+              }}
+            />
+            <InputNumber
+              min={1}
+              type="number"
+              maxLength={8}
+              style={{ margin: "1em 0", width: 100 }}
+              placeholder="< Площадь"
+              onChange={(value) => {
+                setFilter({ ...filter, m22: value })
+              }}
+            />
+          </Input.Group>
+          <Input.Group>
+            <InputNumber
+              min={1}
+              type="number"
+              style={{ margin: "1em 0", width: 100 }}
+              placeholder="Цена >"
+              onChange={(value) => {
+                setFilter({ ...filter, priceFrom: value })
+              }}
+            />
+            <InputNumber
+              min={1}
+              type="number"
+              style={{ margin: "1em 0", width: 100 }}
+              placeholder="< Цена"
+              onChange={(value) => {
+                setFilter({ ...filter, priceTo: value })
+              }}
+            />
+          </Input.Group>
+        </Collapse.Panel>
+      </Collapse>
       <div>
         <Row gutter={16} align="middle">
           {apps.map((app) => {
             return (
               <Col
-                style={{ margin: "1rem 0" }}
+                style={{
+                  margin: "1rem 0",
+                }}
                 xs={{ span: 24 }}
                 sm={{ span: 24 }}
                 md={{ span: 12 }}
